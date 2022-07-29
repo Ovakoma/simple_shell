308a6ec26cead02093f947e076b3362aeae9838a
black-jamesbond <104460484+black-jamesbond@users.noreply.github.com>
2022-07-29 17:00:09 +0100

    h

--- shell.h
+++ shell.h
@@ -4,11 +4,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-<<<<<<< HEAD
 #include <unistd.h>
-=======
-#include <unistd.h>
-#include <sys/types.h>
-
-int _putchar(char c);
->>>>>>> 85f9d7d0e88c18a45567eef02cf821ef15d0613f
