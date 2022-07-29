efb6da688c565df013860e853d35f0398bc77964
Ovakoma <105174945+Ovakoma@users.noreply.github.com>
2022-07-29 16:47:43 +0100

    updated simple shell project

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
 #include "shell.h"
+<<<<<<< HEAD
+
+/**
+ * main - Entry point
+ * @argc: number of arguments passed to function
+ * @argv: arguments passed to function
+ * Return: 0(suceess)
+ */
+int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
+{
+    hsh_loop_func();
+
+    print_st("game");
+    return 0;
+}
+
+=======
+>>>>>>> d4efb9105f072be200729b3dda842e7ad753906e
--- shell.h
+++ shell.h
@@ -1,3 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int _putchar(char c);
