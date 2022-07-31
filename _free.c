f23c1ac12cc4da0d42cf9be8fc420244f1e82954
black-jamesbond <104460484+black-jamesbond@users.noreply.github.com>
2022-07-31 18:09:50 +0100

    Update _free.c

--- _free.c
+++ _free.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
-*
+*_free: it frees up buffers.
+*@c- the buffer to be freed
+*Return - doent return anything
+*/
 
-
-int _free(char **c)
+void _free(char **buff)
 {
     int i;
-    if (!c || c == NULL)
-      return ;
-    while(c[i])
-     {
-      free (c[i]);
-      i++;
-     }
-     free(c);
-     
-   return 0;
+    if (!buff || buff == NULL)
+        return ;
+    while(buff[i])
+    {
+        free (buffi]);
+        i++;
+    }
+    free(buff);
+
 }
