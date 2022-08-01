993acdf3b08c5eed465cc5b4b6f5636ae6254eb9
Ovakoma <105174945+Ovakoma@users.noreply.github.com>
2022-08-01 18:10:17 +0100

    Updated simple shell script project

--- parser.c
+++ parser.c
@@ -12,5 +12,49 @@
     char *tokens, delim[] = " \n", **argv = NULL;
     int n_token = 0, i;
 
-    
+    token = strtok(input, delim);
+    while (token)
+    {
+      n_token++;
+      token = strtok(NULL, delim);
+    }
+    n_token++;
+    argv = malloc(sizeof(char *) * n_token);
+    token = strtok(bufcpy, delim);
+    for (i = 0; token; i++)
+    {
+         argv[i] = malloc(sizeof(char) * _strlen(token));
+         _strcpy(argv[i], token);
+         token = strtok(NULL, delim);
+
+         if (token == NULL)
+         {
+               i++;
+               argv[i] = NULL;
+         }
+    }
+    hsh_exec(argv);
+    free(argv);
+    free(token);
+ }
+
+/**
+ * hsh_exec - executes users parsed input
+ * @argv: argument passed to function
+ */
+ void hsh_exec(char **argv)
+ {
+      pid_t pid = fork();
+
+      if (pid != 0)
+      {
+            wait(NULL);
+            hsh_readline();
+      }
+      else
+      {
+            if (execve(argv[0], argv, NULL) == -1)
+                  perror("No such file or directory");
+      }
+      execve(argv[0], argv, NULL);
  }
--- shell.h
+++ shell.h
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <time.h>
 
+#define TRUE 1
+#define FALSE 0
+
 /* Helper string manipualtion functions */
 char *_strcpy(char *dest, char *str);
 int _strcmp(char *str1, char *str2);
@@ -17,5 +20,7 @@ void _free(char** buff);
 void hsh_readline(void);
 void prompt_user(void);
 int print_st(char *c);
+void hsh_exec(char **argv);
+void parser(char *input, char *bufcpy);
 
 #endif
